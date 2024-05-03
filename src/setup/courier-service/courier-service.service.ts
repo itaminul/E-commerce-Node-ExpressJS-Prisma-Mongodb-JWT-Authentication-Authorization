ca0f08988a8bdedb85977efab85fb122b949b269
itaminul <itaminul@gmail.com>
2024-05-04 00:02:08 +0600

    env example courier service

--- src/setup/courier-service/courier-service.service.ts
+++ src/setup/courier-service/courier-service.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma-connection/primsa.service';
 
 @Injectable()
-export class CourierServiceService {}
+export class CourierServiceService {
+  constructor( private readonly prismaService: PrismaService) {}
+  
+}
