050ef04ea1a1b596c99e842432bad6a1313e5f66
itaminul@gmail.com <itaminul@gmail.com>
2024-05-04 16:43:07 +0600

    redis cache server

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from "./app.service";
 import { CourierServiceModule } from "./setup/courier-service/courier-service.module";
 import { PrismaService } from "./prisma-connection/primsa.service";
 import { ConfigModule } from "@nestjs/config";
+import { CacheModule } from "@nestjs/cache-manager";
+import { redisStore } from "cache-manager-redis-yet";
 
 @Module({
   imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: "localhost",
+            port: 6379,
+          },
+        }),
+      }),
+    }),
     CourierServiceModule,
     ConfigModule.forRoot({
       isGlobal: true,
