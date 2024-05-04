437cc45d36b6c07806908c340547ce324b0bc79b
itaminul@gmail.com <itaminul@gmail.com>
2024-05-04 14:59:13 +0600

    create products and prodcut image table

--- src/setup/courier-service/courier-service.module.ts
+++ src/setup/courier-service/courier-service.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CourierServiceService } from './courier-service.service';
 import { CourierServiceController } from './courier-service.controller';
+import { PrismaService } from 'src/prisma-connection/primsa.service';
 
 @Module({
-  providers: [CourierServiceService],
+  providers: [CourierServiceService, PrismaService],
   controllers: [CourierServiceController]
 })
 export class CourierServiceModule {}
