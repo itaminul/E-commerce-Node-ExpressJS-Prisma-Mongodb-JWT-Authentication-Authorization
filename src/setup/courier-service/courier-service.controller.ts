30f1e48baa42510d1611034f5e7d236512fc35d5
itaminul@gmail.com <itaminul@gmail.com>
2024-05-05 16:29:23 +0600

    get courier service create

--- src/setup/courier-service/courier-service.controller.ts
+++ src/setup/courier-service/courier-service.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, HttpStatus } from "@nestjs/common";
+import { Body, Controller, HttpStatus } from "@nestjs/common";
 import { CourierServiceService } from "./courier-service.service";
+import { CreateCourierServiceDto } from "./dto/create-courier-service.dto";
 
 @Controller("courier-service")
 export class CourierServiceController {
@@ -7,6 +8,18 @@ export class CourierServiceController {
   async getAll() {
     try {
       const results = await this.courierService.getAll();
+      return {
+        success: true,
+        status: HttpStatus.OK,
+        results,
+      };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
+  }
+  async create(@Body() dto: CreateCourierServiceDto) {
+    try {
+      const results = await this.courierService.create(dto);
       return {
         success: true,
         status: HttpStatus.OK,
