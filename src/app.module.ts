1c96162b636eb05b93866d5f5ea9dc5f3c8309b6
itaminul@gmail.com <itaminul@gmail.com>
2024-05-06 18:12:23 +0600

    role base access

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module } from "@nestjs/common";
 import { CourierServiceModule } from "./setup/courier-service/courier-service.module";
 import { PrismaService } from "./prisma-connection/primsa.service";
 import { ConfigModule } from "@nestjs/config";
 import { CacheModule } from "@nestjs/cache-manager";
 import { redisStore } from "cache-manager-redis-yet";
 import { RoleService } from './auth/role/role.service';
+import { AuthMiddleware } from "./auth/auth.middleware";
+import { CourierServiceController } from "./setup/courier-service/courier-service.controller";
 
 @Module({
   imports: [
@@ -25,6 +27,12 @@ import { RoleService } from './auth/role/role.service';
     }),
   ],
   controllers: [],
-  providers: [PrismaService, RoleService, RoleService],
+  providers: [PrismaService, RoleService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes(CourierServiceController); // Specify the route(s) where you want to apply the middleware
+  }
+}
--- src/setup/courier-service/courier-service.module.ts
+++ src/setup/courier-service/courier-service.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CourierServiceService } from './courier-service.service';
 import { CourierServiceController } from './courier-service.controller';
 import { PrismaService } from 'src/prisma-connection/primsa.service';
+import { RoleService } from 'src/auth/role/role.service';
 
 @Module({
-  providers: [CourierServiceService, PrismaService],
+  providers: [CourierServiceService, PrismaService, RoleService],
   controllers: [CourierServiceController]
 })
 export class CourierServiceModule {}
