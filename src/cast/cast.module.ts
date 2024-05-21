5373cca12ab2d038eb2c84dd6649792c23d1ae9d
itaminul <itaminul@gmail.com>
2024-05-21 21:47:07 +0600

    folder and file delete

--- src/cast/cast.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CastService } from './cast.service';
-import { CastController } from './cast.controller';
-
-@Module({
-  providers: [CastService],
-  controllers: [CastController]
-})
-export class CastModule {}
