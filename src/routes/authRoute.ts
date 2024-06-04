d4c4b767e540294bd1f1198efd78b8adec671e11
itaminul@gmail.com <itaminul@gmail.com>
2024-06-04 18:37:51 +0600

    auth role

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { AuthController } from '../controller/auth/authController';
 import { authenticateToken } from '../middleware/authMiddleware';
+import { authenticateToken1,authorizeRoles } from '../middleware/auth';
 const authController = new AuthController();
 const router = Router();
 
@@ -8,4 +9,17 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.get('/check', authenticateToken,authController.check);
 
+// Role-based routes
+router.get('/user', authenticateToken1, authorizeRoles('USER', 'ADMIN', 'SUPER_ADMIN'), (req, res) => {
+    res.send('User route');
+  });
+  
+  router.get('/admin', authenticateToken1, authorizeRoles('ADMIN', 'SUPER_ADMIN'), (req, res) => {
+    res.send('Admin route');
+  });
+  
+  router.get('/super-admin', authenticateToken1, authorizeRoles('SUPER_ADMIN'), (req, res) => {
+    res.send('Super Admin route');
+  });
+  
 export default router;
