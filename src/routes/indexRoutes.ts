a76298659914a59c9d86fbe86a0576f841b53bf9
aminul@atilimited.net <aminul@atilimited.net>
2024-07-07 17:47:11 +0600

    courier route setup

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import authRoute from '../routes/authRoute'
 import productRoute from '../routes/productRoute'
 import storeSetupRoute from '../routes/storeSetupRoute'
+import courierSetupRoute from '../routes/courierRoute'
 const router = express.Router();
 router.use('/auth', authRoute);
 router.use('/product', productRoute)
 router.use('/store-setup', storeSetupRoute)
+router.use('/courier-setup', courierSetupRoute)
 export default router;
