741a6f77b8d0741559cd910182f99e32fa8b486f
aminul@atilimited.net <aminul@atilimited.net>
2024-07-13 15:24:13 +0600

    delete supplier controller

--- src/controller/setup/supplierSetupController.ts
+++ src/controller/setup/supplierSetupController.ts
@@ -21,4 +21,13 @@ export class SupplierSetupController {
       next(error);
     }
   }
+
+  async deleteSupplier(req: Request, res: Response, next: NextFunction) {
+    try {
+      const results = await supplierService.deleteSupplier(req, res, next);
+      res.json({ success: true, results });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
