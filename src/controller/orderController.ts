f2c9e5e4dc0ddb1bad3785bfbe18c89cb32475ef
aminul@atilimited.net <aminul@atilimited.net>
2024-10-07 17:38:50 +0600

    get order

--- src/controller/orderController.ts
+++ src/controller/orderController.ts
@@ -3,17 +3,21 @@ import { OrderServices } from "../services/orderServices";
 
 const orderService = new OrderServices();
 export class OrderController {
+  async getAll(req: Request, res: Response, next: NextFunction) {
+    const results = await orderService.getAll(req, res, next);
+    res.json({ success: true, results });
+  }
   async create(req: Request, res: Response, next: NextFunction) {
     const results = await orderService.create(req, res, next);
     res.json({ success: true, results });
   }
 
   async update(req: Request, res: Response, next: NextFunction) {
-   try {
-    const results = await orderService.update(req, res, next);
-    res.json({ success: true, results });
-   } catch (error) {
-    next(error);
-   }
+    try {
+      const results = await orderService.update(req, res, next);
+      res.json({ success: true, results });
+    } catch (error) {
+      next(error);
+    }
   }
 }
